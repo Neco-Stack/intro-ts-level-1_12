ac52a970c2275543cdb6a5e6770a93657bfc667e
Neco_Stack <necoabay@googlemail.com>
2024-09-29 16:02:34 +0200

    types

---
+++ src/Intro-TS-Level-1_12/basic.ts
@@ -0,0 +1,69 @@
+// let dogName:string = 'Leon';
+// console.log(dogName);
+// dogName = 'elf';
+// console.log(dogName);
+// let seventeen:number = 17; 
+// const sum = seventeen + 44; 
+// console.log(sum);
+
+// Boolean 
+// let a = true; 
+// const b = true; 
+// console.log(a);
+// console.log(b);
+// let d: boolean = true;
+// console.log(d);
+// const c = true; 
+// console.log(c);
+
+// NUMBERS
+// let a:number = 1899;
+// console.log(a);
+// a = 'abi'
+// console.log(a);
+// let b = 1899; 
+// console.log(b);
+// const c = 187654;
+// console.log(c);
+// let d = a < b;
+// console.log(d);
+let a = 'hello';
+console.log(a);
+let b = "billy";
+console.log(b);
+const c = "neco";
+console.log(c);
+let d = a + ' ' + b + ' ' + c;
+console.log(d);
+let e:string = "wassabi";
+console.log(e);
+let f: silly = "silly";
+console.log(f);
+let g:'john' = 'sue'
+console.log(g);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  <a href= './src/Intro-TS-Level-1_12/basic.html'</a>Intro-Ts-Level-1_12
+  </div>
+`
